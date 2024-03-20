9ec3ac0c3ec3dc2eac61015514e71a4b1957cf94
Saqib@Ee <saqib.saleem1@digital.hmrc.gov.uk>
2024-03-20 08:08:39 +0000

    CIR-1391: Some updated files were not formatted correctly

--- src/test/scala/uk/gov/hmrc/test/api/model/response/AssessV3.scala
+++ src/test/scala/uk/gov/hmrc/test/api/model/response/AssessV3.scala
@@ -18,7 +18,16 @@ package uk.gov.hmrc.test.api.model.response
 
 import play.api.libs.json.{Json, OFormat}
 
-case class AssessV3(accountNumberWithSortCodeIsValid: String, accountExists: String, nameMatches: String, nonStandardAccountDetailsRequiredForBacs: String, sortCodeIsPresentOnEISCD: String, sortCodeSupportsDirectDebit: String, sortCodeSupportsDirectCredit: String, sortCodeBankName: Option[String] = None)
+case class AssessV3(
+  accountNumberWithSortCodeIsValid: String,
+  accountExists: String,
+  nameMatches: String,
+  nonStandardAccountDetailsRequiredForBacs: String,
+  sortCodeIsPresentOnEISCD: String,
+  sortCodeSupportsDirectDebit: String,
+  sortCodeSupportsDirectCredit: String,
+  sortCodeBankName: Option[String] = None
+)
 
 object AssessV3 {
   implicit val responseJsonFormat: OFormat[AssessV3] = Json.format[AssessV3]
