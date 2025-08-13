d7ef08af814f1f9f662639e84ac8372faef686df
MJCallahanPage <16954819+MJCallahanPage@users.noreply.github.com>
2025-08-13 15:21:16 +0100

    [CIR-2151] Other dep bumps and unuse dimports

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,14 +2,16 @@ import sbt.*
 
 object Dependencies {
 
+  private val bouncyCastleVersion = "1.81"
+
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "api-test-runner"          % "0.10.0",
     "org.slf4j"               % "slf4j-api"                 % "2.0.17",
-    "software.amazon.awssdk"  % "s3"                        % "2.32.10",
-    "org.mock-server"         % "mockserver-netty"          % "5.12.0",
-    "io.swagger.parser.v3"    % "swagger-parser"            % "2.1.18",
+    "software.amazon.awssdk"  % "s3"                        % "2.32.20",
+    "org.mock-server"         % "mockserver-netty"          % "5.15.0",
+    "io.swagger.parser.v3"    % "swagger-parser"            % "2.1.31",
     "org.openapi4j"           % "openapi-schema-validator"  % "1.0.7",
-    "org.bouncycastle"        % "bcprov-jdk18on"            % "1.81",
-    "org.bouncycastle"        % "bcpkix-jdk18on"            % "1.81"
+    "org.bouncycastle"        % "bcprov-jdk18on"            % bouncyCastleVersion,
+    "org.bouncycastle"        % "bcpkix-jdk18on"            % bouncyCastleVersion
   ).map(_ % Test)
 }
--- src/test/scala/uk/gov/hmrc/test/api/specs/SwaggerSpecs.scala
+++ src/test/scala/uk/gov/hmrc/test/api/specs/SwaggerSpecs.scala
@@ -26,12 +26,10 @@ import org.openapi4j.schema.validator.ValidationData
 import org.openapi4j.schema.validator.v3.SchemaValidator
 import org.scalatest.AppendedClues.convertToClueful
 import org.scalatest.matchers.should.Matchers._
-import org.scalatest.time.SpanSugar.convertIntToGrainOfTime
 import org.scalatest.wordspec.AnyWordSpec
 import uk.gov.hmrc.test.api.conf.TestConfiguration
 import uk.gov.hmrc.test.api.service.BankAccountReputationService
 
-import scala.concurrent.Await
 import scala.jdk.StreamConverters._
 
 class SwaggerSpecs extends AnyWordSpec with SwaggerSpec {
